583b8680cc0f9e5422f3d9fee06a9fe549cc1959
carlo8pastor <120664886+carlo8pastor@users.noreply.github.com>
2023-10-15 18:41:57 +0200

    Carlo comments for utils.dart

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,3 +1,5 @@
 String formatDate(DateTime dateTime) {
   return "${dateTime.year}-${dateTime.month.toString().padLeft(2, '0')}-${dateTime.day.toString().padLeft(2, '0')} ${dateTime.hour.toString().padLeft(2, '0')}:${dateTime.minute.toString().padLeft(2, '0')}";
 }
+
+// Carlo: Possibly use the intl package, which provides date and time formatting.
